bf1e5142f20c1821e95efaff7c72c78739fff64c
Victor <victorgit28@outlook.com>
2024-12-05 10:10:29 -0300

    Ajustando conteudo

--- js/menu.js
+++
@@ -1,7 +0,0 @@
-const menuButton = document.getElementById('menuButton');
-        const navMenu = document.getElementById('navMenu');
-        
-        menuButton.addEventListener('click', () => {
-          menuButton.classList.toggle('active');
-          navMenu.classList.toggle('active');
-        });
